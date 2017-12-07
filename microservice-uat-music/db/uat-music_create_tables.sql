1650cb17f4791d56e1bcc017fb4cea7ecfcf9ebb
ukimiawz <melisa.cecilia@yahoo.com>
2017-12-07 11:30:47 +0100

    Added microservice uat-music

---
+++ microservice-uat-music/db/uat-music_create_tables.sql
@@ -0,0 +1,19 @@
+--
+-- Database Schema:  uatTest
+-- Automatically generated sql script for the service uat-music, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table tblMusic.
+--
+CREATE TABLE uatTest.tblMusic (
+  imageId INT(11) ,
+  musicId INT(11) ,
+  musicName VARCHAR(255) ,
+  musicUrl VARCHAR(255) ,
+CONSTRAINT musicId_PK PRIMARY KEY (musicId)
+ 
+);
+
+
+
